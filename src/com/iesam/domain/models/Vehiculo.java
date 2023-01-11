088e52aa068c2d7646a70a378369927b295e33f3
rdiaazz_3 <rocio.diafer.1@educa.jcyl.es>
2023-01-11 01:50:45 +0100

    Revert "Examen ITV terminado"
    This reverts commit 5e793fcb120fbe214e81c50f5632da1678140608.

--- src/com/iesam/domain/models/Vehiculo.java
+++ src/com/iesam/domain/models/Vehiculo.java
@@ -2,9 +2,10 @@ package com.iesam.domain.models;
 
 public interface Vehiculo {
 
-
-    public String getCodigo();
+    public String getCodVehiculo();
     public String getMarca();
     public String getModelo();
+    public String getColor();
+    public String getAñoM();
 
 }
--- src/com/iesam/presentation/PrintInspeccion.java
+++ src/com/iesam/presentation/PrintInspeccion.java
@@ -6,13 +6,8 @@ import com.iesam.domain.models.Inspeccion;
 public class PrintInspeccion {
 
 
-    public void imprimir (Inspeccion inspeccion){
-        System.out.println("CodInspección: "+ inspeccion.getCodigo("77"));
-        System.out.println("Resultado de la inspección: "+ inspeccion.getResultado());
-        System.out.println("Fecha de realización: "+ inspeccion.getFechaInspeccion("29-06-22"));
-        System.out.println("Propietario del Vehículo: "+ inspeccion.getPropietario().getAdress()+"");
-        System.out.println("Vehiculo inspeccionado: "+ inspeccion.getVehiculo().getCodigo());
-        System.out.println("_______________________________________________________________");
+    public static void main(String [] args ){
 
+        System.out.println("Codigo de inspección:_______________________"+);
     }}
 
